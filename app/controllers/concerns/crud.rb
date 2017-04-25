9c0d21cef12bc7f5c40e5cc9a7c66b2f91c38620
Nick Herro <ncherro@gmail.com>
2017-04-25 00:04:11 -0400

    set up our crud views

--- app/controllers/concerns/crud.rb
+++ app/controllers/concerns/crud.rb
@@ -1,26 +1,83 @@
-module Controllers
-  # Crud concerns
-  module Crud
-    extend ActiveSupport::Concern
+# CRUD ops for controllers
+module Crud
+  extend ActiveSupport::Concern
 
-    def index
-      klass.ordered.page(params[:page])
-    end
-
-    def show
-    end
-
-    def update
-    end
-
-    def destroy
-    end
-
-    private
-
-    def current_object
-      @current_object ||= klass.find(params[:id])
-    end
+  included do
+    helper_method :current_set
     helper_method :current_object
+  end
+
+  def index; end
+
+  def new
+    @current_object = klass.new
+  end
+
+  def create
+    @current_object = klass.new(safe_params)
+
+    if current_object.save
+      redirect_to(
+        index_path,
+        notice: notice('Successfully created!')
+      )
+    else
+      render :new, alert: 'There was an error'
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if current_object.update(safe_params)
+      format.html do
+        redirect_to(
+          index_path,
+          notice: notice("Successfully updated#{name}!")
+        )
+      end
+    else
+      format.html { render :edit, alert: 'There was an error' }
+    end
+  end
+
+  def destroy
+    if current_object.destroy
+      redirect_to(
+        index_path,
+        notice: "Successfully deleted#{current_object.name}!"
+      )
+    else
+      redirect_to :back, alert: current_object.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def index_path
+    { action: :index }
+  end
+
+  def notice(message)
+    %(#{message} <a class="btn btn-xs btn-default" href="#{url_for(action: :edit, id: current_object)}"><i class="fa fa-pencil"></i> Edit again</a>)
+  end
+
+  def current_set
+    @current_set ||= begin
+                       query = apply_scopes(klass)
+                       query = query.ordered
+                       extend_current_set(query.page(params[:page]))
+                     end
+  end
+
+  def extend_current_set(query)
+    # override to chain stuff to the query
+    query
+  end
+
+  def current_object
+    @current_object ||= klass.find(params[:id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,47 @@
 class PostsController < ApplicationController
-  include Controllers::Crud
-end
+  helper_method :klass
 
+  has_scope :title
+  has_scope :category_ids, type: :array
+  has_scope :tag_ids, type: :array
+
+  include Crud
+
+  class << self
+    def filters
+      {
+        title: {
+          type: :string,
+          label: 'Title'
+        },
+        tag_ids: {
+          type: :select,
+          label: 'Tags',
+          collection: Tag.pluck(:id, :name),
+          multiple: true
+        },
+        category_ids: {
+          type: :select,
+          label: 'Categories',
+          collection: Category.pluck(:id, :name),
+          multiple: true
+        }
+      }
+    end
+  end
+
+  private
+
+  def safe_params
+    params.require(:post).permit(
+      :title,
+      :text,
+      :category_id,
+      tag_ids: []
+    )
+  end
+
+  def klass
+    Post
+  end
+end
